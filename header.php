244b9196eee56c09cd3d2e7df041016388a02a5a
AJ Foster <web@aj-foster.com>
2013-11-01 15:54:51 -0400

    Add cityscape, adjust nav for one-page layout.

--- header.php
+++ header.php
@@ -3,12 +3,13 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
-		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+		<script type="text/javascript" src="http://use.typekit.net/tkc8rlv.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	</head>
 <?php
 	global $post;
@@ -23,10 +24,10 @@
 		<div class="page">
 			<header>
 				<nav class="main-menu full">
-					<div class="screen-reader-text skip-link"><a href="#UPDATE ME" title="Skip to content">Skip to content</a></div>
+					<div class="cityscape"></div>
+					<div class="screen-reader-text skip-link"><a href="#feature" title="Skip to content">Skip to content</a></div>
 					<div class="compact-menu">
-						<a class="menu-toggle">Menu</a>
-						<?php get_search_form(); ?>
+						<a href="#" class="menu-toggle">Menu</a>
 					</div>
 					<ul>
 <?php
@@ -40,7 +41,7 @@
 								$navLoop->the_post();
 
 								$name = get_the_title();
-								$link = get_permalink();
+								$link = "#" . $post->post_name;
 								$nav_li_class = (get_the_ID() == $current_ID) ? ' class="current" ' : '';
 
 								if (get_post_meta($post->ID, 'page-form-visible', true) == 'show') {
@@ -49,11 +50,6 @@
 						<li<?php echo $nav_li_class; ?>><a href="<?php echo $link; ?>"><?php echo $name; ?></a></li>
 <?php 							}
 							} ?>
-						<li>
-							<form method="get" id="searchform" class="searchform" action="http://localhost/wp/" role="search">
-								<input type="search" class="field" name="s" value="" id="s" placeholder="Search &#133;" />
-							</form>
-						</li>
 					</ul>
 				</nav>
 			</header>
